281309436d5ed102a37a271ddfcf0abedd40fcab
Dan Garfield <dangarfielduk@hotmail.com>
2020-12-01 21:04:25 +0000

    Initial commit

---
+++ convert-files.js
@@ -0,0 +1,165 @@
+const pandoc = require('node-pandoc-promise')
+const fs = require('fs-extra')
+const path = require('path')
+const glob = require('glob-promise')
+
+const MEDIAWIKI_DIR_PAGES = './output/mediawiki/pages'
+const MEDIAWIKI_DIR_ASSETS = './output/mediawiki/assets'
+
+const PANDOC_DIR_PAGES = './output/pandoc/pages'
+const OUTPUT_DIR_PAGES = './output/markdown/pages'
+
+const OUTPUT_DIR_ASSETS = './output/markdown/assets'
+
+const getMediaWikiPageNames = async () => {
+  const mediaWikiPageNames = await glob(`**/*.mw`, {cwd: MEDIAWIKI_DIR_PAGES})
+  console.log('mediaWikiPageNames', mediaWikiPageNames)
+  return mediaWikiPageNames
+}
+const convertToMarkdown = async () => {
+  const mediaWikiPageNames = await getMediaWikiPageNames()
+  await fs.remove(PANDOC_DIR_PAGES)
+  const convertedPageNames = []
+  for (let i = 0; i < mediaWikiPageNames.length; i++) {
+    //   for (let i = 0; i < 5; i++) {
+    const mediaWikiPageName = mediaWikiPageNames[i]
+    const markdownPageName = mediaWikiPageName.replace('.mw', '.md')
+    console.log('Converting', (i + 1), 'of', mediaWikiPageNames.length, ' - ', mediaWikiPageName)
+    const mediaWikiFile = path.join(MEDIAWIKI_DIR_PAGES, mediaWikiPageName)
+    const markdownFile = path.join(PANDOC_DIR_PAGES, markdownPageName)
+    const markdownDir = path.dirname(markdownFile)
+    // console.log('markdownFile', mediaWikiFile, '->', markdownFile, 'in', markdownDir)
+    await fs.ensureDir(markdownDir)
+    await pandoc(mediaWikiFile, ['-f', 'mediawiki', '-t', 'gfm', '-o', markdownFile, '--reference-links'])
+    convertedPageNames.push(markdownPageName)
+  }
+  return convertedPageNames
+}
+
+const getMarkdownPageNames = async () => {
+  const markdownPageNames = await glob(`**/*.md`, {cwd: PANDOC_DIR_PAGES})
+  //   console.log('markdownPageNames', markdownPageNames)
+  return markdownPageNames
+}
+const isImage = (fileName) => {
+  if (fileName.includes('.')) {
+    const ext = fileName.toLowerCase().substring(fileName.lastIndexOf('.') + 1)
+    switch (ext) {
+      case 'png': case 'jpeg': case 'jpg':
+        return true
+    }
+  }
+  return false
+}
+const cleanLink = (link) => {
+  if (link.startsWith('#')) {
+    link = `#user-content-${link.toLowerCase().replace(/[ _]/g, '-').replace('#', '')}`
+  } else if (!link.startsWith('http')) {
+    if (isImage(link)) {
+      link = '/assets/' + link
+    } else {
+      link = '/pages/' + link
+      if (link.includes('#')) {
+        const hashIndex = link.indexOf('#')
+        const hash = link.substring(hashIndex)
+        link = `${link.substring(0, hashIndex)}.md${hash}`
+      } else {
+        link = link + '.md'
+      }
+    }
+    link = link.replace(/_/g, '%20')
+  }
+  return link
+}
+const amendHTMLLink = (page) => {
+  const regex = /href="([^"]*)"/g
+  let match
+  let replacements = []
+  while ((match = regex.exec(page)) !== null) {
+    if (match.index === regex.lastIndex) {
+      regex.lastIndex++
+    }
+    const wholeLink = match[0]
+    const href = match[1]
+    const link = cleanLink(href)
+    // console.log('amendHTMLLink', wholeLink, href, link)
+    replacements.push({replace: wholeLink, with: `href="${link}"`})
+  }
+  for (let i = 0; i < replacements.length; i++) {
+    const replacement = replacements
+    page = page.replace(replacement[i].replace, replacement[i].with)
+  }
+  return page
+}
+
+const amendLink = (page) => {
+  const regex = /(\[.+\]:) (.+)/g
+  let match
+  let replacements = []
+  while ((match = regex.exec(page)) !== null) {
+    if (match.index === regex.lastIndex) {
+      regex.lastIndex++
+    }
+    const wholeLink = match[0]
+    const linkTitle = match[1]
+    const initialLinkAndTitle = match[2]
+    let updatedLinkAndTitle = initialLinkAndTitle
+    let title = ''
+    if (updatedLinkAndTitle.includes('"')) {
+      const t1 = updatedLinkAndTitle.indexOf('"')
+      const t2 = updatedLinkAndTitle.lastIndexOf('"')
+      title = updatedLinkAndTitle.substring(t1, t2 + 1)
+      updatedLinkAndTitle = updatedLinkAndTitle.substring(0, t1 - 1)
+    //   console.log('t1', t1, t2, title, '-' + updatedLinkAndTitle + '-')
+    }
+    updatedLinkAndTitle = cleanLink(updatedLinkAndTitle)
+
+    if (title.length > 0) {
+      updatedLinkAndTitle = `${updatedLinkAndTitle} ${title}`
+    }
+
+    const updatedWholeLink = `${linkTitle} ${updatedLinkAndTitle}`
+    // console.log('match', wholeLink, '-', initialLinkAndTitle, '->', updatedWholeLink)
+    replacements.push({replace: wholeLink, with: updatedWholeLink})
+  }
+  for (let i = 0; i < replacements.length; i++) {
+    const replacement = replacements
+    page = page.replace(replacement[i].replace, replacement[i].with)
+  }
+
+  //   console.log('page', page)
+  return page
+}
+const amendLinks = async () => {
+  await fs.remove(OUTPUT_DIR_PAGES)
+  const markdownPageNames = await getMarkdownPageNames()
+  for (let i = 0; i < markdownPageNames.length; i++) {
+    const markdownPageName = markdownPageNames[i]
+    // console.log('Amending links', (i + 1), 'of', markdownPageNames.length, ' - ', markdownPageName)
+    const pandocFile = path.join(PANDOC_DIR_PAGES, markdownPageName)
+    const markdownFile = path.join(OUTPUT_DIR_PAGES, markdownPageName)
+    const content = await fs.readFile(pandocFile, 'utf8')
+    const updatedContent = amendHTMLLink(amendLink(content))
+    const markdownDir = path.dirname(markdownFile)
+    await fs.ensureDir(markdownDir)
+    await fs.writeFile(markdownFile, updatedContent)
+  }
+}
+const moveAssets = async () => {
+  await fs.remove(OUTPUT_DIR_ASSETS)
+  await fs.ensureDir(OUTPUT_DIR_ASSETS)
+  await fs.copy(MEDIAWIKI_DIR_ASSETS, OUTPUT_DIR_ASSETS)
+}
+const init = async () => {
+  try {
+    console.log('Converting to markdown: START')
+    // await convertToMarkdown()
+    await moveAssets()
+    await amendLinks()
+    console.log('Converting to markdown: END')
+  } catch (error) {
+    console.error(`Converting to markdown: ERROR - ${error.message}`)
+  }
+}
+
+init()
---
+++ download-files.js
@@ -0,0 +1,88 @@
+const cheerio = require('cheerio')
+const got = require('got')
+const fs = require('fs-extra')
+const path = require('path')
+const stream = require('stream')
+const { promisify } = require('util')
+const pipeline = promisify(stream.pipeline)
+
+const DOMAIN = 'https://wiki.ffrtt.ru'
+const SCRAPE_URL_PAGES = `${DOMAIN}/index.php?title=Special:AllPages`
+const EXPORT_URL_PAGES = `${DOMAIN}/index.php?title=Special:Export/`
+const EXPORT_URL_ASSETS = `${DOMAIN}/index.php?title=Special:ListFiles`
+
+const OUTPUT_DIR_PAGES = './output/mediawiki/pages'
+const OUTPUT_DIR_ASSETS = './output/mediawiki/assets'
+
+const scrapePage = async (url, list) => {
+  console.log('scrapePage', url)
+  const response = await got(url)
+  let nextUrl = false
+  const $ = cheerio.load(response.body)
+  $('.mw-allpages-body a').each((i, link) => {
+    const title = link.attribs.href.replace('/index.php?title=', '')
+    list.push(title)
+  })
+  $('.mw-allpages-nav a:contains("Next page")').first().each((i, link) => {
+    nextUrl = DOMAIN + link.attribs.href
+  })
+  return nextUrl
+}
+const getAllPageNames = async () => {
+  let nextUrl = SCRAPE_URL_PAGES
+  const nameList = []
+  do {
+    nextUrl = await scrapePage(nextUrl, nameList)
+  } while (nextUrl)
+  return nameList
+}
+const downloadPages = async (allPageNames) => {
+  await fs.remove(OUTPUT_DIR_PAGES)
+  for (let i = 0; i < allPageNames.length; i++) {
+    //   for (let i = 0; i < 7; i++) {
+    console.log('Download Pages', (i + 1), 'of', allPageNames.length)
+    const pageName = allPageNames[i]
+    const response = await got(EXPORT_URL_PAGES + pageName)
+    const $ = cheerio.load(response.body, {xmlMode: true})
+    const title = $('mediawiki page title').text()
+    const body = $('mediawiki page revision text').text()
+    const file = path.join(OUTPUT_DIR_PAGES, `${title}.mw`)
+    const dir = path.dirname(file)
+    await fs.ensureDir(dir)
+    await fs.writeFile(file, body)
+    console.log(title, dir, file)
+  }
+}
+const downloadFiles = async () => {
+  await fs.remove(OUTPUT_DIR_ASSETS)
+
+  const response = await got(EXPORT_URL_ASSETS)
+  const $ = cheerio.load(response.body) // Note: No pagination needed, only one page
+  console.log($('title').text())
+
+  $('.TablePager_col_img_name').each(async (i, cellEle) => {
+    const links = $(cellEle).find('a').toArray()
+    const fileName = links[0].attribs.title.replace('File:', '')
+    const file = path.join(OUTPUT_DIR_ASSETS, fileName)
+    const dir = path.dirname(file)
+    await fs.ensureDir(dir)
+    const url = DOMAIN + links[1].attribs.href
+    console.log('file', fileName, url)
+
+    await pipeline(got.stream(url), fs.createWriteStream(file))
+    console.log(`File downloaded to ${fileName}`)
+  })
+}
+const init = async () => {
+  console.log('Downloading wiki data: START')
+  try {
+    const allPageNames = await getAllPageNames()
+    await downloadPages(allPageNames)
+    await downloadFiles()
+    console.log('Downloading wiki data: START')
+  } catch (error) {
+    console.error(`Downloading wiki data: ERROR - ${error.message}`)
+  }
+}
+
+init()
